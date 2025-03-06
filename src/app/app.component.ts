5bb1777e7ffba9dd5b5aaad8abc58338d5a4c13c
Raphael Laaz <raphaellaaz01@gmail.com>
2025-03-06 09:39:24 -0500

    fix: Added selector feat: Added uso de funcion de servicios para probar conexcion con la API, datos recibidos correctamente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IngredientesServiceService } from './services/ingredientes-service.service';
+
 
 @Component({
-  selector: 'app-root',
   imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontanimal';
+
+
+  constructor(private apiIngredientes: IngredientesServiceService) {}
+
+  ngOnInit() {
+    this.apiIngredientes.getAllIngredientes().subscribe(
+      (data) => {
+        console.log('Datos recibidos:', data);
+      },
+      (error) => {
+        console.error('Error en la conexión:', error);
+      }
+    );
+  }
+  
+  
+
 }
